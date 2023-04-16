c85452f235e21e35048d2f0a96f027d581554e15
JulinhaBarros <barros.flaviojulia@gmail.com>
2023-04-16 19:08:31 -0300

    git

---
+++ projeto.js
@@ -0,0 +1,312 @@
+const bd = `{
+        "alunos" : [
+        {
+            "nome": "João",
+            "rgm": 268,
+            "avaliacaoParcial": 0,
+            "exercicio": 2,
+            "avaliacaoRegimental": 5,
+            "media": 0,
+            "Status": null,
+            "img": "./imgs/joao.jpeg"
+            },
+        {
+            "nome": "Melissa",
+            "rgm": 279,
+            "avaliacaoParcial": 5,
+            "exercicio": 2,
+            "avaliacaoRegimental": 2,
+            "media": 0,
+            "Status": null,
+            "img": "./imgs/Melissa.jpeg"
+        },
+        {
+            "nome": "Jessica",
+            "rgm": 217,
+            "avaliacaoParcial": 0,
+            "exercicio": 1,
+            "avaliacaoRegimental": 1,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Jessica.jpeg"
+        },
+        {
+            "nome": "Victoria",
+            "rgm": 287,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 4,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Victoria.jpeg"
+        },
+        {
+            "nome": "Logan",
+            "rgm": 218,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 2,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Logan.jpeg"
+        },
+        {
+            "nome": "Sabrina",
+            "rgm": 201,
+            "avaliacaoParcial": 1,
+            "exercicio": 1,
+            "avaliacaoRegimental": 1,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Sabrina.jpeg"
+        },
+        {
+            "nome": "Lena",
+            "rgm": 214,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 5,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Lena.jpeg"
+        },
+        {
+            "nome": "Elizabeth",
+            "rgm": 280,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 2,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Elizabeth.jpeg"
+        },
+        {
+            "nome": "William",
+            "rgm": 259,
+            "avaliacaoParcial": 0,
+            "exercicio": 0,
+            "avaliacaoRegimental": 5,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/William.jpeg"
+        },
+        {
+            "nome": "Emilly",
+            "rgm": 249,
+            "avaliacaoParcial": 1,
+            "exercicio": 1,
+            "avaliacaoRegimental": 1,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Emilly.jpeg"
+        },
+        {
+            "nome": "Matteo",
+            "rgm": 293,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 5,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Matteo.jpeg"
+        },
+        {
+            "nome": "Érika",
+            "rgm": 286,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 3,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Érika.jpeg"
+        },
+        {
+            "nome": "Alex",
+            "rgm": 210,
+            "avaliacaoParcial": 1,
+            "exercicio": 1,
+            "avaliacaoRegimental": 2,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Alex.jpeg"
+        },
+        {
+            "nome": "Rafael",
+            "rgm": 290,
+            "avaliacaoParcial": 1,
+            "exercicio": 1,
+            "avaliacaoRegimental": 0,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Rafael.jpeg"
+        },
+        {
+            "nome": "Amelie",
+            "rgm": 288,
+            "avaliacaoParcial": 0,
+            "exercicio": 1,
+            "avaliacaoRegimental": 5,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Amelie.jpeg"
+        },
+        {
+            "nome": "Jason",
+            "rgm": 285,
+            "avaliacaoParcial": 2,
+            "exercicio": 2,
+            "avaliacaoRegimental": 4,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Jason.jpeg"
+        },
+        {
+            "nome": "Maria",
+            "rgm": 223,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 3,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Maria.jpeg"
+        },
+        {
+            "nome": "Fernanda",
+            "rgm": 226,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 5,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Fernanda.jpeg"
+        },
+        {
+            "nome": "Jonas",
+            "rgm": 282,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 0,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/jonas.jpeg"
+        },
+        {
+            "nome": "Julie",
+            "rgm": 281,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 5,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Julie.jpeg"
+        },
+        {
+            "nome": "Scott",
+            "rgm": 295,
+            "avaliacaoParcial": 3,
+            "exercicio": 1,
+            "avaliacaoRegimental": 5,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Scott.jpeg"
+        },
+        {
+            "nome": "Antonio",
+            "rgm": 287,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 3,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Antonio.jpeg"
+        },
+        {
+            "nome": "Jack",
+            "rgm": 278,
+            "avaliacaoParcial": 2,
+            "exercicio": 0,
+            "avaliacaoRegimental": 0,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Jack.jpeg"
+        },
+        {
+            "nome": "Jake",
+            "rgm": 202,
+            "avaliacaoParcial": 1,
+            "exercicio": 1,
+            "avaliacaoRegimental": 5,
+            "img": "./imgs/Jake.jpeg"
+        },
+        {
+            "nome": "Amy",
+            "rgm": 250,
+            "avaliacaoParcial": 2,
+            "exercicio": 1,
+            "avaliacaoRegimental": 2,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Amy.jpeg"
+        },
+        {
+            "nome": "Taylor",
+            "rgm": 214,
+            "avaliacaoParcial": 0,
+            "exercicio": 1,
+            "avaliacaoRegimental": 0,
+            "media": null,
+            "Status": null,
+            "img": "./imgs/Taylor.jpeg"
+        }
+    ]
+}`
+
+const objs = JSON.parse(bd);
+const dados = []
+const media = []
+
+objs.alunos.forEach(element => {
+    
+    dados.push(element);
+    media.push(element.media = element.avaliacaoParcial+ element.exercicio + element.avaliacaoRegimental)/3;
+
+    for(element.Status of dados){
+    if(element.media >= 6) {
+        element.Status = "Aprovado";
+        
+    }
+    else if ( element.media < 6 && element.media >= 2) {
+        element.Status = "Recuperação";
+    }
+    else {
+        element.Status = "Reprovado";
+    }
+
+    }
+});
+
+
+function exibirAlunos() {
+    document.getElementById("container").innerHTML = "";
+
+    dados.forEach(function (elem, projeto) {
+    var list = document.getElementById('body');
+    let info = document.createElement('info');
+    
+    info.innerHTML = `
+    
+    <ul>
+    <img src= "${dados[projeto].img}" style="width: 130px; height: 130px" />
+    <li><b>Nome:</b> ${dados[projeto].nome}</li>
+    <li><b>RGM:</b> ${dados[projeto].rgm}</li>
+    <li><b>Av.Parcial:</b> ${dados[projeto].avaliacaoParcial}</li>
+    <li><b>Exercicios:</b> ${dados[projeto].exercicio}</li>
+    <li><b>Av.Regimental:</b> ${dados[projeto].avaliacaoRegimental}</li>
+    <li><b>Média:</b> ${dados[projeto].media}</b></li>
+    <li><b>Status:</b> ${dados[projeto].Status}</b></li>
+    </ul>`
+    list.appendChild(info)
+});  
+
+}
+
